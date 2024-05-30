a3bd1dcea42f10d4b4614b158f0d0f09ad18793d
Dm1tryb1t <afanasevdima465@gmail.com>
2024-05-30 10:44:32 +0300

    add hellowrld

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -2,9 +2,16 @@
 #include <string> 
 
 int main(){
+<<<<<<< HEAD
 string name; //comment
 cout << "enter name: " << endl;
 cin >> name;
 cout << "Hello world " << name << endl;
+=======
+string name;
+std::cout << "enter name: " << std::endl;
+std::cin >> name;
+std::cout << "Hello world " << name << std::endl;
+>>>>>>> 8186bc1 (add hellowrld)
 }
 
